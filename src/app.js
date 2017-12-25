14d7e01bd4ed1e6d853f8b25d28e98db32c5c3c9
NoureldeanSaed <Noureldean.saed@gmail.com>
2017-12-25 19:38:46 +0200

    Put almost the complete inital struture

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const controller = require('./controllers/controller.js');
+const exphbs = require('express-handlebars');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+  })
+);
+app.use(controller);
+
+module.exports = app;
---
+++ src/controllers/login.js
@@ -0,0 +1,3 @@
+exports.get = (req, res) => {
+  res.status(200).send('<h1> We are at login page</h1>');
+};
